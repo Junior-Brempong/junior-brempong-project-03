94c74e56bf3f1ed7d67ca34ee6fc599acdc78b4c
junior-brempong <juniorwilliam222@gmail.com>
2023-01-05 14:00:33 -0500

    Added scroll to top button

--- src/App.js
+++ src/App.js
@@ -9,12 +9,45 @@ import Footer from './components/Footer';
 
 
 function App() {
+
+    // Get the button:
+  let mybutton = document.getElementById("backToTopButton");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+  } else {
+      mybutton.style.display = "none";
+  }
+
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+
+   
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });
+
+  }
+
+
   return (
     <div className="App">
       <Title/>
       <ImageForm />
       <ImageDisplay />
       <Footer/>
+
+      <a id='backToTopButton' onClick={topFunction}>
+        <p>Back to top!</p>
+      </a>
     </div>
   );
 }
