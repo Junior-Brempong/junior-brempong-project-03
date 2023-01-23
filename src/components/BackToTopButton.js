2bd17b2e8107f73085e487544f67257e66cb42dd
junior-brempong <juniorwilliam222@gmail.com>
2023-01-23 15:50:06 -0500

    added pseudo logic

--- src/components/BackToTopButton.js
+++ src/components/BackToTopButton.js
@@ -3,13 +3,13 @@ import { useEffect } from 'react';
 
 const BackToTopButton = () => {
 
-    // We make sure that our button has already been rendered to the page before we apply our logic, that is why we use a useEffect
+    // Step 1: We make sure that our button has already been rendered to the page before we apply our logic, that is why we use a useEffect
 
     useEffect(() => {
-        // Grab the button
+        // Step 1(a): Grab the button element from our return
         let mybutton = document.getElementById("backToTopButton");
 
-        // Function that shows our button if we scroll more than 20px down the page and makes it disappear if we are less than that
+        // Step 1(b) Create a Function that shows our button if we scroll more than 20px down the page and makes it disappear if we are less than that
         window.onscroll = function() {
           if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
             mybutton.style.display = "block";
@@ -19,7 +19,7 @@ const BackToTopButton = () => {
         }
       }, []);
     
-      // When the user clicks on the button, scroll to the top of the document
+      // Step 2: When the user clicks on the button, scroll to the top of the document which we pass through to our button JSX onClick
       function topFunction() {
            
       window.scrollTo({
